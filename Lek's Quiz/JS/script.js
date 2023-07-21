d9ed27f896e66e2de5f27bf88e0be0caeb3fd029
Keoma Silva <keomakds@hotmail.com>
2023-07-21 00:28:08 -0300

    fix: form password

--- Lek's Quiz/JS/script.js
+++ Lek's Quiz/JS/script.js
@@ -1,34 +1,32 @@
 const form = document.getElementById('formRegister');
-const nickInput = document.getElementById('nickName');
-const emailInput = document.getElementById('contatemail');
-const passqordInput = document.getElementById('password');
+const passwordInput = document.getElementById('passwordRegister');
 const confirmPasswordInput = document.getElementById('confirmPassword');
 
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    if(nickInput.value === ""){
-        alert("Sem Nick");
-        return;
-    }
-    if(emailInput.value === "" || !emailValid(emailInput.value)){
-        SpeechRecognitionAlternative("Preencha o email");
-        return;
-    }
-
-
-
-
-
-    form.submit();
-})
-function emailValid(email){
-    const emailRegex = new RegExp{
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-    };
-    if(emailRegex.test(email)){
+function validadePassword(passwordInput, minDigits){
+    if(passwordInput.length >= minDigits) {
         return true;
     }
     return false;
 }
+function samePassword(passwordInput, confirmPasswordInput){
+    if(passwordInput.value === confirmPasswordInput){
+        return true;
+    }
+    passwordInput.classList.add('invalid');
+    confirmPasswordInput.classList.add('invalid');
+    return false;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(!validadePassword(passwordInput.value,8)){
+        passwordInput.classList.add('invalid');
+        return
+    }
+    if(!samePassword(passwordInput, confirmPasswordInput)){
+        return
+    }
+
+    form.submit();
+})
